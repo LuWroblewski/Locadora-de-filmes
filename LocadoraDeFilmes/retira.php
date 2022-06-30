43b74b5d4920a176074817f3e121e4893f3fbc94
Small-Lexy <92353062+Small-Lexy@users.noreply.github.com>
2022-06-30 15:28:29 -0300

    Update retira.php

--- LocadoraDeFilmes/retira.php
+++ LocadoraDeFilmes/retira.php
@@ -43,10 +43,10 @@
         $y = $_POST["IDcliente"];
         $z = $_POST["dataRetira"];
         $w = $_POST["dataDev"];
-        $endereço = '167.99.252.245';
-        $usuario = 'BSI_E4';
-        $senha = 'bsi@e42022';
-        $bd = 'BSI_E4_LU';
+        $endereço = '';
+        $usuario = '';
+        $senha = '';
+        $bd = '';
 
         $connect = mysqli_connect($endereço, $usuario, $senha, $bd);
 
